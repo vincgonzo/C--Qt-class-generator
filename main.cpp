f1a81e8601f453d7f6b83ceb757bf5671ab89cca
vincgonzo@hotmail.fr <vincgonzo@hotmail.fr>
2019-12-16 13:14:10 +0100

    try groupbox multiple add

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 #include <QApplication>
-#include <QWidget>
-#include <QPushButton>
-#include <QDialog>
-#include <QLabel>
-#include <QVBoxLayout>
-#include <QRadioButton>
-#include <QGroupBox>
-#include <QFormLayout>
-#include <QLineEdit>
+#include <QtWidgets>
+
 #include "ClassGenerator.h"
 
 int main(int argc, char *argv[])
@@ -15,38 +8,42 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QWidget wind;
+    QLineEdit *nom = new QLineEdit;
+    QLineEdit *prenom = new QLineEdit;
+    QLineEdit *age = new QLineEdit;
+
+    QVBoxLayout *layoutMain = new QVBoxLayout;
+
     // Options layout
     QGroupBox *gpBox = new QGroupBox("Class Definition", &wind);
-    QGroupBox *gpBoxOpt = new QGroupBox("Options", &wind);
-
     QRadioButton *header_include = new QRadioButton("Protéger le header contre les inclusions multiples.");
     QRadioButton *construct_def = new QRadioButton("Créer un constructeur par défaut.");
     QRadioButton *destruct = new QRadioButton("Générer un destructeur.");
 
     header_include->setChecked(true);
 
-    QVBoxLayout *vboxOpt = new QVBoxLayout;
-    vboxOpt->addWidget(header_include);
-    vboxOpt->addWidget(construct_def);
-    vboxOpt->addWidget(destruct);
+    QVBoxLayout *layoutDef = new QVBoxLayout;
+    layoutDef->addWidget(header_include);
+    layoutDef->addWidget(construct_def);
+    layoutDef->addWidget(destruct);
+
+    // Attrib def section to group
+    //gpBox->setLayout(layoutDef);
 
 
-
-    QLineEdit *nom = new QLineEdit;
-    QLineEdit *prenom = new QLineEdit;
-    QLineEdit *age = new QLineEdit;
-
-
-    QFormLayout *layout = new QFormLayout;
-    layout->addRow("Votre nom", nom);
-    layout->addRow("Votre prénom", prenom);
-    layout->addRow("Votre âge", age);
+    QGroupBox *gpBoxOpt = new QGroupBox("Options", &wind);
+    QFormLayout *layoutForm = new QFormLayout;
+    layoutForm->addRow("Votre &nom", nom);
+    layoutForm->addRow("Votre &prénom", prenom);
+    layoutForm->addRow("Votre &age", age);
 
     // Container injections
-    gpBox->setLayout(vboxOpt);
+    //gpBoxOpt->setLayout(layoutForm);
 
-    gpBoxOpt->setLayout(layout);
+    layoutMain->addLayout(layoutDef);
+    layoutMain->addLayout(layoutForm);
 
+    wind.setLayout(layoutMain);
     wind.show();
     //QWidget::connect(bouton, SIGNAL(clicked()), &secondeFenetre, SLOT(exec()));
 
